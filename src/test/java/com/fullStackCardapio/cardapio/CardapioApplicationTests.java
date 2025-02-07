181eb04f8fdc8b4641940f73ac9642efe871197b
Bruno Araujo <brunnoaraujoc@gmail.com>
2025-02-07 11:24:08 -0300

    Inital commit

---
+++ src/test/java/com/fullStackCardapio/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStackCardapio.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
